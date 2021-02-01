124ce7d4503cfd004da4b43a7175064c0e25e8c8
Leonardo Santana <leonardo.santana4025@gmail.com>
2021-02-01 16:13:26 -0300

    Primeira versão do projeto

---
+++ src/app/photo/photo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "lp-photo",
+    templateUrl: 'photo.component.html'
+})
+
+export class PhotoComponent {
+    description = 'The Las of Us Part II';
+    url = 'https://images-na.ssl-images-amazon.com/images/I/61OZeW%2BpPYL._AC_SL1000_.jpg'
+}
